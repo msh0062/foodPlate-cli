bfbf69ad3bc617a7ac839d7ae9d00c9d0f1fe33a
Mason Hassen <mshassen08@yahoo.com>
2021-08-23 12:52:41 -0500

    Current changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'fp-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {}

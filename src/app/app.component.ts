365b089f7a2886fa1e1517236ce7176a6bc194b5
Mason Hassen <mshassen08@yahoo.com>
2021-08-23 09:55:13 -0500

    footer added

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'fp-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+
+})
+export class AppComponent {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [ AppComponent, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    BrowserModule
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
